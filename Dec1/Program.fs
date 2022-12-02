2d436ee633179e1c5d75c914dd4b3d4a799920b0
Akira Yorumoto <yorumoto.akira@outlook.com>
2022-12-02 05:12:48 +0100

    Day 1

---
+++ Dec1/Program.fs
@@ -0,0 +1,27 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+open System.IO
+
+let filename: string = "input.txt"
+
+let file: FileInfo = new FileInfo(filename)
+
+let contents: string[] =
+    using (file.OpenText()) (fun (reader: StreamReader) -> reader.ReadToEnd().Split "\n\n")
+
+let caloriesOfElves: int[] =
+    contents
+    |> Array.map (fun (elfCalories: string) ->
+        let calories: int =
+            elfCalories.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+            |> Array.map int
+            |> Array.sum
+
+        calories)
+
+let biggestElf: int = caloriesOfElves |> Array.max
+
+let topThreeElves: int =
+    caloriesOfElves |> Seq.sortDescending |> Seq.take 3 |> Seq.sum
+
+printfn "Biggest: %d, TopThree: %d" biggestElf topThreeElves
